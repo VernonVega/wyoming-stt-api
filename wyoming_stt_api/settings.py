a949a5002999beff24c6b06730d44f83bb3f1f50
VernonVega <mld.sphere@gmail.com>
2026-01-13 02:06:35 +0300

    Update settings.py

--- wyoming_stt_api/settings.py
+++ wyoming_stt_api/settings.py
@@ -2,11 +2,10 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    openai_api_key: str
-    # See link for available model names:
-    # https://platform.openai.com/docs/api-reference/audio/createTranscription
-    openai_model: str = "gpt-4o-mini-transcribe"
-
+    #url for private text to speech service
+    ats_url: str
+    
+    
     server_host: str = "0.0.0.0"
     server_port: int = 10300
 

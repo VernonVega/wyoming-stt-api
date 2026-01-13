3945a356b6a3ba42168bfa607f41c9e4c20af4d2
VernonVega <mld.sphere@gmail.com>
2026-01-13 13:42:04 +0300

    Update ats.py

--- wyoming_stt_api/clients/ats.py
+++ wyoming_stt_api/clients/ats.py
@@ -42,7 +42,7 @@ class ATSClient:
     def model_name(self) -> str:
         return "gigaam_refined"
 
-    def parse_ats_response(text: str) -> str:
+    def parse_ats_response(self, text: str) -> str:
         """Parse ATS response which may be:
            * a JSON array of objects
            * concatenated JSON objects without delimiters
